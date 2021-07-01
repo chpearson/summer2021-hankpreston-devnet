5f7d50dd362a7ea9756b98f0e74e702b0a930e79
Chuck Pearson <cpearson3@gmail.com>
2021-07-01 09:44:34 -0700

    error handling and raw data collection on error

--- network_inventory.py
+++ network_inventory.py
@@ -7,6 +7,7 @@ Goal:
     device name, software version, uptime, serial number
 """
 from pyats.topology.loader import load
+from genie.libs.parser.utils.common import ParserNotFound
 
 #Script entry point
 if __name__ == "__main__":
@@ -27,7 +28,38 @@ if __name__ == "__main__":
     testbed.connect(log_stdout=False)
 
     # Run commands to gather output from devices
+    show_version = {}
+    show_inventory = {}
 
+    def parse_command(device, command):
+        """
+        Attempt to parse a command on a device with pyATS.
+        In case of common errors, return best info possible.
+        """
+
+        print(f"Running {command} on {device.name}")
+        try:
+            output = device.parse(command)
+            return {"type": "parsed", "output": output}
+        except ParserNotFound:
+            print(f" Error: pyATS lacks a parser for device")
+
+        # device.execute runs commnd, gathers raw output
+        output = device.execute(command)
+        return {"type": "raw", "output": output}
+
+###################
+
+    for device in testbed.devices:
+        print(f"Gathering show version from device {device}")
+        show_version[device] = parse_command(testbed.devices[device], 'show version')
+
+        print(f"{device} show version: {show_version[device]}")
+
+        print(f"Gathering show inventory from device {device}")
+        show_inventory[device] = testbed.devices[device].parse('show inventory')
+
+        print(f"{device} show inventory: {show_inventory[device]}")
     # Disconnect from network devices
     for device in testbed.devices:
         print(f'Disconnecting from device {device}.')
